36ae721eebe6b4566d82b63349dcb0126b31e478
deatona404 <deatona404@vt.edu>
2022-10-24 15:21:43 -0400

    longle now works in vscode

--- src/lngl/ProjectRunner.java
+++ src/lngl/ProjectRunner.java
@@ -1,6 +1,7 @@
 package longle.src.lngl;
 
 import java.util.Scanner;
+import java.io.*;
 /**
  * Runs the Longle project.
  */
@@ -14,8 +15,8 @@ public class ProjectRunner {
         System.out.println("What length word would you like to guess?");
         int desiredLength = input.nextInt();
         input.nextLine();
-        String fileToSearch = "./longle/src/" + desiredLength + "Letter.txt";
-        
+        String fileToSearch = "./wordLists/" + desiredLength + "Letter.txt";
+        // System.out.println(new File("testDeletelaterr").getAbsolutePath());
         // Initialize game and word selector
         WordSelect selector = new WordSelect(fileToSearch);
         String word = selector.getWord();
@@ -24,6 +25,13 @@ public class ProjectRunner {
 
         // allows the user to guess until they run out of guesses :)
         boolean unsolved = true;
+
+        System.out.println("KEY:");
+        System.out.println("@ correct letter in correct location");
+        System.out.println("% correct letter, wrong location");
+        System.out.println("# letter does not exist in word");
+        
+
         while(unsolved){
             System.out.println("LEN " + word.length() + " | guess "+ (game.getGuesses() + 1) + ": ");
             String currGuess = input.nextLine();
